6cbff9cbe62ed0148988b7f8b934d00ea83cdff1
alexandretavanocardoso <AlexandreTavanoDeveloper@outlook.com>
2022-12-19 21:11:13 -0300

    html page notfound

--- src/FrontEnd/studio-flavia-begosso/src/components/_comuns/notFound/notFound.vue
+++ src/FrontEnd/studio-flavia-begosso/src/components/_comuns/notFound/notFound.vue
@@ -1,6 +1,9 @@
 <template>
-    <img src="@/assets/img/cilios.png" />
-    <h4>404</h4>
+    <div class="flex">
+        <h4>Desculpe, esta página não está disponível.</h4>
+        <p>O link que você seguiu pode estar quebrado ou a página pode ter sido removida.</p>
+        <img src="@/assets/img/cilios.jpg" />
+    </div>
 </template> 
 
 <script>
@@ -9,4 +12,6 @@ export default {
 }
 </script>
 
-<style src="./notFound.scss" lang="scss" scoped></style>
+<style src="./notFound.scss" lang="scss" scoped>
+
+</style>
